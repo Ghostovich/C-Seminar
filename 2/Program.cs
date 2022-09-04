158018f293d9c95118999dc2277bc5d6918bef9d
Konstantin <gusarov_konstant@mail.ru>
2022-09-04 13:37:01 +0300

    seminar1

---
+++ 2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите числа (предлагаемый квадрат числа и само число):");
+int x = int.Parse(Console.ReadLine());
+int y = int.Parse(Console.ReadLine());
+
+int z = y*y;
+if (x==z) {
+    Console.Write("1 число является квадратом второго");
+}
+
+else {
+    Console.Write("1 число не является квадратом второго");
+}
