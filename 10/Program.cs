e17e33c79baa5f6466421039707b5fcc5911e42f
Konstantin <gusarov_konstant@mail.ru>
2022-09-11 15:50:06 +0300

    04

--- 10/Program.cs
+++ 10/Program.cs
@@ -1,10 +1,32 @@
-﻿
- Console.Write("Введите координаты число:");
-int n = int.Parse(Console.ReadLine());
-  
-  for (int i=1; i<=n; i++){
-        int k = i*i;
-        Console.WriteLine(k);
-    }
+﻿int GetSum (int number)
+{
+  int sum = 0;
+  if (number>0){
+  for (int i = 1; i<=number; i++)
+  {
+    sum = sum + i;
+  }
+  }
+    if (number<0){
+  for (int i = 1; i>=number; i--)
+  {
+    sum += i;
+  }
+  }
+  return sum;
+}
 
+
+Console.Write("Введите число:");
+
+try {
+int a = int.Parse(Console.ReadLine());
+  
+int sum = GetSum(a);
+Console.Write(sum);
+}
+
+catch (Exception ex){
+Console.Write($"Неверный формат. Ошибка: {ex.Message}");  
+}
 
---
+++ 11/Program.cs
@@ -0,0 +1,10 @@
+﻿
+ Console.Write("Введите координаты число:");
+int n = int.Parse(Console.ReadLine());
+  
+  for (int i=1; i<=n; i++){
+        int k = i*i;
+        Console.WriteLine(k);
+    }
+
+
