a54afef9282c69c703f949c315abe64987889030
Konstantin <gusarov_konstant@mail.ru>
2022-09-21 08:12:08 +0300

    05.1

---
+++ 18/Program.cs
@@ -0,0 +1,22 @@
+﻿int GetSide () base (side1, side2, side3)
+{
+    int side1 = new Random().Next(1,10);
+    int side2 = new Random().Next(1,10);
+    int side3 = new Random().Next(1,10);
+    return new (side1, side2, side3);
+}
+
+// int[] reverse(int[] array)
+// {
+//     int[] newArray = new int[array.Length];
+//     for (int i = 0; i<array.Length; i++)
+//     {
+//         newArray[i] = array[array.Length-i-1];
+//     }
+//     return newArray;
+// }
+
+int side = GetSide();
+Console.WriteLine(string.Join(", ", side));
+// int[] newArr = reverse(arr);
+// Console.WriteLine(string.Join(", ", newArr));
