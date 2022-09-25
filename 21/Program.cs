f7169d66ace9a83187ecd0eab8d62a28e95a9784
Konstantin <gusarov_konstant@mail.ru>
2022-09-25 21:53:03 +0300

    07.2

---
+++ 21/Program.cs
@@ -0,0 +1,37 @@
+﻿int[,] GetArray (int n, int m)
+{
+    int[,] array = new int[n, m];
+     for (int i=0; i<n;i++)
+    {
+        for (int j=0; j<m; j++)
+        {
+           array [i,j] = new Random().Next(-10,10);
+        }
+    }
+    for (int i=0; i<n;i++)
+    {
+        for (int j=0; j<m; j++)
+        {
+            if (i%2==0 && j%2 ==0)
+            {
+                 array [i,j]= array [i,j]* array [i,j];
+            }
+        }
+    }   
+    return array;
+}
+
+Console.WriteLine("Введите количество столбцов:");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество строк:");
+int m = int.Parse(Console.ReadLine());
+
+int[,] arr = GetArray(n,m);
+for (int i=0; i<n;i++)
+{
+    for (int j=0; j<m; j++)
+    {
+        Console.Write(arr [i,j]+" ");
+    }
+    Console.WriteLine();
+}
---
+++ 22/Program.cs
@@ -0,0 +1,45 @@
+﻿int[,] GetArray (int n, int m)
+{
+    int[,] array = new int[n, m];
+    for (int i=0; i<n;i++)
+    {
+        for (int j=0; j<m; j++)
+        {
+           array [i,j] = new Random().Next(-10,10);
+        }
+    }
+    return array;
+}
+
+int GetSum(int[,] arr, int n, int m)
+{
+    int sum = 0;
+    for (int i=0; i<n;i++)
+    {
+        for (int j=0; j<m; j++)
+        {
+            if (i ==j)
+            {
+                sum = sum+arr [i,j];
+            }
+        }
+    }   
+    return sum;
+}
+
+Console.WriteLine("Введите количество столбцов:");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество строк:");
+int m = int.Parse(Console.ReadLine());
+
+int[,] arr = GetArray(n,m);
+for (int i=0; i<n;i++)
+{
+    for (int j=0; j<m; j++)
+    {
+        Console.Write(arr [i,j]+" ");
+    }
+    Console.WriteLine();
+}
+int printSum = GetSum(arr,n, m);
+Console.WriteLine(printSum);
